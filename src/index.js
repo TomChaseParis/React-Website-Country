1e4b1daa6116f6c6e83e361985caab94c92c02fe
TomChaseParis <chasethomasparis@gmail.com>
2022-04-04 08:10:33 +0200

    Ready to build Application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render( // Nous rend la balise App
   <React.StrictMode>
-    <App />
+    <App /> {/* De App va découler toute notre application */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // On passe toute l'application à root
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
